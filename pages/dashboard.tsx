77dead88cbbb91c5162983b7435ef9a13f488ceb
Gustavo <gustavopolonio1@gmail.com>
2022-05-24 10:48:08 -0300

    validate cookies for auth

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,6 +1,7 @@
 import { useContext, useEffect } from 'react'
 import { AuthContext } from '../contexts/AuthContext'
 import { api } from '../services/api'
+import { withSSRAuth } from '../utils/withSSRAuth'
 
 export default function Dashboar() {
   const { user } = useContext(AuthContext)
@@ -15,3 +16,10 @@ export default function Dashboar() {
     <h1>Dashboard: {user?.email}</h1>
   )
 }
+
+export const getServerSideProps = withSSRAuth(async (ctx) => {
+
+  return {
+    props: {}
+  }
+})
